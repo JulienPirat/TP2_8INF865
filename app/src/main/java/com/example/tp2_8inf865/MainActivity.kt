b7727fff9c7a0a3bce148f5e107ed1b7bfbc303a
JulienPirat <72934794+JulienPirat@users.noreply.github.com>
2023-10-02 19:42:27 -0400

    [INIT]

---
+++ app/src/main/java/com/example/tp2_8inf865/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.tp2_8inf865
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tp2_8inf865.ui.theme.TP2_8INF865Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TP2_8INF865Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TP2_8INF865Theme {
+        Greeting("Android")
+    }
+}
